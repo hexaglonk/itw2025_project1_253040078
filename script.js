b1e6fcbc8c4acae617ed939c21557ae261d70c7c
hexaglonk <ramsg030@gmail.com>
2025-12-28 00:07:23 +0700

    konek spreadsheet

--- script.js
+++ script.js
@@ -12,3 +12,33 @@ document.querySelectorAll(".nav-links a").forEach((n) =>
     navMenu.classList.remove("active");
   })
 );
+
+const scriptURL = "https://script.google.com/macros/s/AKfycbwMnZOBSJKc4z5d6m-OO9SsXVzlLTI8-mD4p7pJQVHzCZ6sa91OtQ7hP3yxLqwbAT-8/exec";
+const form = document.forms["submit-to-google-sheet"];
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => console.log("Success!", response))
+    .catch((error) => console.error("Error!", error.message));
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const contactForm = document.querySelector(".contact-form");
+  const submitBtn = document.querySelector("#submit-btn");
+  if (contactForm && submitBtn) {
+    contactForm.addEventListener("submit", function (e) {
+      e.preventDefault();
+      submitBtn.classList.add("loading");
+      submitBtn.disabled = true;
+
+      setTimeout(() => {
+        submitBtn.classList.remove("loading");
+        submitBtn.disabled = false;
+
+        alert("Pesan Terkirim!");
+        contactForm.reset();
+      }, 2000);
+    });
+  }
+});
