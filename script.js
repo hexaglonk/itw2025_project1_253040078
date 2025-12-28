c1c434426fd1fd67d3c7cbb3a1d4955d0d3f4f9d
hexaglonk <ramsg030@gmail.com>
2025-12-28 22:32:54 +0700

    rapihkan margin pada project

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ document.addEventListener("DOMContentLoaded", () => {
         submitBtn.classList.remove("loading");
         submitBtn.disabled = false;
 
-        alert("Pesan Terkirim!");
+        alert("Pesan Anda Telah Terkirim Terimakasih!");
         contactForm.reset();
       }, 2000);
     });
